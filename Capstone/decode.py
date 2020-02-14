28b33fe1ccd9407cb44262a1cfc53f9459c31e29
davidrharvey <david.harvey@mymail.champlain.edu>
2020-02-14 11:23:52 -0500

    Updated decode and added 5k model

--- Capstone/decode.py
+++ Capstone/decode.py
@@ -1,74 +1,56 @@
-import cv2 as cv
+import pandas as pd 
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import urllib
-import json
 import pickle
-import requests
+import cv2 as cv
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
-from flask import request
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def getMatches():
-    # Masquerade as Mozilla because some web servers may not like python bots.
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    # Set up the request
-    #req = urllib.Request(request.vars.url, headers=hdr)
-    url = request.args.get('url')
-    req = urllib.request.urlopen(url, headers=hdr)
 
 
-    try:
-        # Load the image as parameter
-        # Obtain the content of the url
-        con = urllib.urlopen( req )
-        # Read the content and convert it into an numpy array
-        im_array = np.asarray(bytearray(con.read()), dtype=np.uint8)
-        # Convert the numpy array into an image.
-        image =  cv.imdecode(im_array, cv.IMREAD_UNCHANGED)
+# Load the image as parameter from POST args
+#image = cv.imread("000120.jpg") # Error with face not looking at camera
+#image = cv.imread("DavidKopec.jpg") # Same cluster is always returned
+image = cv.imread("000001.jpg") # Same cluster is always returned
 
-        # Stop CvtHelper errors with RBG cutoff
-        #image = image.astype('uint8')
+# Read the Features - To be updated to facemark, still using ORB atm
+d = []
 
-        # Read the Features - To be updated to facemark
-        X_test = pd.DataFrame(columns=['Name','X_Coord', 'Y_Coord'])
+# create facemark detector and load lbf model:
+facemark = cv.face.createFacemarkLBF()
+facemark.loadModel("lbfmodel.yaml")
 
-        detector = cv.ORB_create(nfeatures=20)
-        key = detector.detect(cv.cvtColor(image, cv.COLOR_BGR2GRAY))
-        for p in key:
-            name = "0.jpg"
-            x = p.pt[0]
-            y = p.pt[1]
-            X_test = X_test.append({'Name': name,'X_Coord': x, 'Y_Coord': y}, ignore_index = True)
+# load cascade detector
+cascade = cv.CascadeClassifier('C:/OpenCV/sources/data/haarcascades/haarcascade_frontalface_alt.xml')
+faces = cascade.detectMultiScale(image, 1.3, 5)
 
+ok, landmarks = facemark.fit(image, faces)
 
-        # Run Label Encoder Over the Data
-        for i in X_test:
-            le = LabelEncoder()
+imageName = "test.jpg"
+if ok:
+    for marks in landmarks[0]:
+        for mark in marks:
+            d.append({'X': mark[0], 'Y': mark[1]})
+                        
+else:
+    print("Landmark DETECTION failed")
 
-            le.fit(X_test[i])
-            X_test[i] = le.transform(X_test[i])
+X_test = pd.DataFrame(d)
+# Run Label Encoder Over the Data
+for i in X_test:
+    le = LabelEncoder()
+    le.fit(X_test[i])
+    X_test[i] = le.transform(X_test[i])
 
-        # de-serialize mlp_nn.pkl file into an object called mlp_nn using pickle
-        with open('/home/davidrharvey/web2py/applications/celhair/uploads/model.pkl', 'rb') as handle:
-            kmeans = pickle.load(handle)
+# de-serialize static model.pkl file into an object called kmeans using pickle
+with open('model_facemark_5k_noname.pkl', 'rb') as model:
+    kmeans = pickle.load(model)
 
-        # Let X_test be the feature (UNIX timestamp) for which we want to predict the output
-        result = kmeans.predict(X_test)
-        result_cluster = np.where(kmeans.labels_ == result[0])[0]
+# Let X_test be the feature for which we want to predict the output
+result = kmeans.predict(X_test)
+result_cluster = np.where(kmeans.labels_ == result[0])[0]
 
-        result_images = []
-        for i in range(40):
-            result_images.append(str(result_cluster[i]).zfill(6) + ".jpg")
+result_images = []
+for i in range(20):
+    result_images.append(str(result_cluster[i]).zfill(6) + ".jpg")
 
-        # Return names of result cluster image in json dump
-        return json.dumps(result_images)
-
-
-    except urllib.HTTPError as e:
-        return e.fp.read()
+print(result_images)
--- Capstone/facemark_test.py
+++ Capstone/facemark_test.py
@@ -9,14 +9,13 @@ from os import listdir
 from os.path import isfile, join
 
 # Load Image Features - Running OpenCV Feature dectection over every file in given directory
-mypath = "test_set"
+#mypath = "test_set"
+mypath = "training_images_large"
 
-
-# Creating dataframe
-df = pd.DataFrame(columns=[])
-
+d = []
 # Open cascade classifier:
 cascade = cv.CascadeClassifier('C:/OpenCV/sources/data/haarcascades/haarcascade_frontalface_alt.xml')
+#cascade = cv.CascadeClassifier('C:/OpenCV/sources/data/lbpcascades/lbpcascade_frontalface_improved.xml')
 
 # create facemark detector and load lbf model:
 facemark = cv.face.createFacemarkLBF()
@@ -26,26 +25,31 @@ onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 images = np.empty(len(onlyfiles), dtype=object)
 for n in range(0, len(onlyfiles)):
     # Load Image
-    print("Loading Features From images: ", str(n+1).zfill(6) + ".jpg")
     images[n] = cv.imread( join(mypath,onlyfiles[n]) )
+    imageName = str(n+1).zfill(6) + ".jpg"
 
+    try:
+        # Run landmark detector:
+        faces = cascade.detectMultiScale(images[n], 1.3, 5)
+        ok, landmarks = facemark.fit(images[n], faces)
+        # Extract Landmark data 
+        if ok:
+            for marks in landmarks[0]:
+                for mark in marks:
+                    d.append({'X': mark[0], 'Y': mark[1]})
+                        
+        else:
+            print("Landmark DETECTION failed on image: ", imageName)
+    except:
+        print("Landmark APPEND failed on image: ", str(n+1).zfill(6) + ".jpg")
 
-    # Run landmark detector:
-    faces = cascade.detectMultiScale(images[n], 1.3, 5)
-    ok, landmarks = facemark.fit(images[n], faces)
-    if(not ok):
-        print("Landmark detection error on image: ", str(n+1).zfill(6) + ".jpg")
-
-
-    print("Facial Landmarks:", landmarks)
-    # Append Landmark data to dataframe
-    df = df.append(landmarks)
-
+# Turn data into dataframe for kmeans
+df = pd.DataFrame(d)
 
 # Run Label Encoder Over the Data
+print("Done loading images!")
 for i in df:
     le = LabelEncoder()
-
     le.fit(df[i])
     df[i] = le.transform(df[i])
 
@@ -53,8 +57,7 @@ for i in df:
 kmeans = KMeans(n_clusters=20, random_state=69, n_init=50, precompute_distances='auto', algorithm='auto').fit(df)
 centroids = np.array(kmeans.cluster_centers_)
 
-print("Centriods: ", centroids)
 
 # Dump Model to Pickle File
-with open('model_facemark.pkl', 'wb') as model_file:
+with open('model_facemark_5k_noname.pkl', 'wb') as model_file:
   pickle.dump(kmeans, model_file, protocol=2)
